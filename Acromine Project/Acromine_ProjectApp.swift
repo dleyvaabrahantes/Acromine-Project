990f809fc5c0a8a3cfc7a0bc9e2f90e928469555
David Leyva <dleyvaabrahantes@gmail.com>
2021-11-05 08:22:59 -0500

    Initial Commit

---
+++ Acromine Project/Acromine_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Acromine_ProjectApp.swift
+//  Acromine Project
+//
+//  Created by David on 11/5/21.
+//
+
+import SwiftUI
+
+@main
+struct Acromine_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
