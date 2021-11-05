360db1eb4d0502ef44389621fe305dc1acc3d488
David Leyva <dleyvaabrahantes@gmail.com>
2021-11-05 14:44:51 -0500

    Add unit test request

--- Acromine ProjectTests/Acromine_ProjectTests.swift
+++ Acromine ProjectTests/Acromine_ProjectTests.swift
@@ -9,13 +9,19 @@ import XCTest
 @testable import Acromine_Project
 
 class Acromine_ProjectTests: XCTestCase {
+   
+    var sut: URLSession!
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+          sut = URLSession(configuration: .default)
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+          try super.tearDownWithError()
     }
 
     func testExample() throws {
@@ -28,6 +34,35 @@ class Acromine_ProjectTests: XCTestCase {
         self.measure {
             // Put the code you want to measure the time of here.
         }
+    }
+    
+    // Asynchronous test: success fast, failure slow
+    func testValidApiCallGetsHTTPStatusCode200() throws {
+      // given
+      let urlString =
+        "http://www.nactem.ac.uk/software/acromine/dictionary.py?sf=dla&lf="
+      let url = URL(string: urlString)!
+      // 1
+      let promise = expectation(description: "Status code: 200")
+
+      // when
+      let dataTask = sut.dataTask(with: url) { _, response, error in
+        // then
+        if let error = error {
+          XCTFail("Error: \(error.localizedDescription)")
+          return
+        } else if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+          if statusCode == 200 {
+            // 2
+            promise.fulfill()
+          } else {
+            XCTFail("Status code: \(statusCode)")
+          }
+        }
+      }
+      dataTask.resume()
+      // 3
+      wait(for: [promise], timeout: 5)
     }
 
 }
